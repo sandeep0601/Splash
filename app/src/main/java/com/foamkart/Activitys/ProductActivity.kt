1a004f0b92e051bb45eb5b6b1198671af4dccd6b
sandeep0601 <76037271+sandeep0601@users.noreply.github.com>
2020-12-22 18:18:46 +0530

    add snackbar in productActivity

--- app/src/main/java/com/foamkart/Activitys/ProductActivity.kt
+++ app/src/main/java/com/foamkart/Activitys/ProductActivity.kt
@@ -1,12 +1,16 @@
 package com.foamkart.Activitys
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import com.foamkart.Adapter.ProductSliderAdapter
 import com.foamkart.Adapter.ViewPagerAdapter
 import com.foamkart.R
 import com.foamkart.databinding.ActivitySingleProductBinding
+import com.google.android.material.snackbar.Snackbar
+
 
 class ProductActivity : AppCompatActivity() {
     lateinit var binding: ActivitySingleProductBinding
@@ -19,9 +23,29 @@ class ProductActivity : AppCompatActivity() {
         binding.title.setText("iPhone 10x pro.")
         binding.imgBack.setOnClickListener { finish() }
 
+
+        binding.imgFavorite.setOnClickListener {
+            var snackbar = Snackbar
+                .make(it, "Selected", Snackbar.LENGTH_LONG)
+            snackbar.show()
+            binding.imgFavorite.setImageResource(R.drawable.ic_baseline_favorite_border_redcolor)
+
+  /*          snackBar.setActionTextColor(Color.BLUE)
+            val snackBarView: View = snackBar.getView()
+            val textView: TextView =
+                snackBarView.findViewById(android.support.design.R.id.snackbar_text)
+            textView.setTextColor(Color.RED)
+            snackBar.show()*/
+
+        }
+
     }
     fun setSlider() {
-        var images = intArrayOf(R.drawable.mobile_image, R.drawable.mobile_image, R.drawable.mobile_image)
+        var images = intArrayOf(
+            R.drawable.mobile_image,
+            R.drawable.mobile_image,
+            R.drawable.mobile_image
+        )
         binding.viewpagerSlider.adapter = ViewPagerAdapter(this, images)
     }
 }
